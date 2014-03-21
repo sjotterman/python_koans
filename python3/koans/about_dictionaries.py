a715c7a81d15d9928ee3efbd23abc67e46256de3
Samuel Otterman <sjotterman@gmail.com>
2014-03-21 11:15:03 -0400

    Working on about_dictionaries. Completed 49 koans and 5 lessons

--- python3/koans/about_dictionaries.py
+++ python3/koans/about_dictionaries.py
@@ -29,14 +29,14 @@ class AboutDictionaries(Koan):
         babel_fish = { 'one': 'uno', 'two': 'dos' }
         babel_fish['one'] = 'eins'
 
-        expected = { 'two': 'dos', 'one': __ }
+        expected = { 'two': 'dos', 'one': 'eins' }
         self.assertDictEqual(expected, babel_fish)
 
     def test_dictionary_is_unordered(self):
         dict1 = { 'one': 'uno', 'two': 'dos' }
         dict2 = { 'two': 'dos', 'one': 'uno' }
 
-        self.assertEqual(__, dict1 == dict2)
+        self.assertEqual(True, dict1 == dict2)
 
 
     def test_dictionary_keys_and_values(self):
