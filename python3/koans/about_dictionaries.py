1f4cc78fd3d20329f97ff6a7de27a9b01b48140b
Samuel Otterman <sjotterman@gmail.com>
2014-04-01 22:59:14 -0400

    about_dictionarie - finished 49 koans and 5 lessons

--- python3/koans/about_dictionaries.py
+++ python3/koans/about_dictionaries.py
@@ -36,7 +36,7 @@ class AboutDictionaries(Koan):
         dict1 = { 'one': 'uno', 'two': 'dos' }
         dict2 = { 'two': 'dos', 'one': 'uno' }
 
-        self.assertEqual(__, dict1 == dict2)
+        self.assertEqual(True, dict1 == dict2)
 
 
     def test_dictionary_keys_and_values(self):
